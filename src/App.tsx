de6645ec293538d4acf2260dba4b77ec809729c6
vicsantizo <santizo.vic@gmail.com>
2022-11-29 20:09:40 -0600

    feat: Display components and add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import { Route, Routes, Outlet } from 'react-router-dom';
+import { Dropdown } from './dropdown';
+import { Header } from './header';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import { Sidebar, useSidebar } from './sidebar';
+import { Toggle } from './toggle';
+
+const AppLayout = () => {
+  const { isSidebarActive, setIsSidebarActive } = useSidebar();
+  return (
+    <>
+      <Header isSidebarActive={isSidebarActive} setIsSidebarActive={setIsSidebarActive} />
+      <Sidebar isSidebarActive={isSidebarActive}>
+        <Dropdown text="Settings">
+          <Toggle text="Dark mode" />
+        </Dropdown>
+      </Sidebar>
+      <Outlet />
+    </>
+  );
+};
+
 function App() {
-  return <div className="App">Custom Create React App</div>;
+  return (
+    <>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import './css/home.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div>Home</div>
+    </div>
+  );
+};
+
+export default Home;
