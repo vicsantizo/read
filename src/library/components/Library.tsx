eb26535f53c000cab5f347427835511e5b962a0c
vicsantizo <santizo.vic@gmail.com>
2022-12-01 12:17:39 -0600

    feat: Create Library Component

--- src/library/components/Library.tsx
+++ src/library/components/Library.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import { useBooksStore } from '../dataStores/useBookStore';
+import { Book } from './Book';
+import EditButton from './EditButton';
+import MenuButton from './MenuButton';
+import AddButton from './AddButton';
+import Search from './Search';
 
 export const Library = () => {
-  return <div>Library</div>;
+  const { data, error, createBook } = useBooksStore();
+  return (
+    <div className="library">
+      <h1 className="library__title font-bold text-[1.25rem] text-center mb-4 mt-5 ">My Library</h1>
+      <div className="library__filter flex justify-center align-center mt-5 mb-5">
+        <Search />
+      </div>
+      <div className="library__actions flex gap-3 justify-center mb-6 items-center">
+        <MenuButton />
+        <EditButton />
+        <AddButton />
+      </div>
+      <div className="library__body">
+        <div className="library__books flex gap-3 flex-wrap justify-center">
+          {data.map((book) => (
+            <Book key={book.getIdentifier()} title={book.getTitle()} author={book.getAuthor()} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Library;
