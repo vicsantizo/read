aad8c24ef12b127b88f6dadebb0108ce571c92ba
vicsantizo <santizo.vic@gmail.com>
2022-12-09 01:05:01 -0600

    chore: add deleteBook method to interface

--- src/library/dataStores/IBookPersistentStorage.ts
+++ src/library/dataStores/IBookPersistentStorage.ts
@@ -3,4 +3,5 @@ import { Book } from '../models';
 export interface IBookPersistentStorage {
   fetchAllBooks: () => Promise<Book[]>;
   saveAllBooks: (books: Book[]) => Promise<void>;
+  deleteBook: (ids: Record<string, boolean>) => Promise<Book[]>;
 }
