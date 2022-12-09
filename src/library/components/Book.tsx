d94d803acceab98fd86b1c3cc2430a6c3d26b0d8
vicsantizo <santizo.vic@gmail.com>
2022-12-09 01:16:14 -0600

    chore: Add a checkbox to select books

--- src/library/components/Book.tsx
+++ src/library/components/Book.tsx
@@ -1,14 +1,29 @@
+import { MutableRefObject } from 'react';
 import './css/book.css';
 
 type BookProps = {
   title: string;
   author: string;
+  bookId: string;
+  booksSelected: MutableRefObject<Record<string, boolean>>;
+  editionMode: boolean;
 };
 
 export const Book = (props: BookProps) => {
-  const { title, author } = props;
+  const { title, author, bookId, editionMode, booksSelected } = props;
   return (
-    <div className="book">
+    <div className="book relative">
+      <div className="absolute top-0 right-0 mr-2 mt-2 flex items-center gap-2">
+        <input
+          className={`${
+            editionMode ? 'visible' : 'invisible'
+          } h-[1.5rem] w-[1.5rem] md:h-[1.25rem] md:w-[1.25rem] z-[1] mr-auto`}
+          type={'checkbox'}
+          onChange={(e) => {
+            booksSelected.current[bookId] = e.target.checked;
+          }}
+        />
+      </div>
       <span className="book__title">{title}</span>
       <span className="book__author">{author}</span>
     </div>
