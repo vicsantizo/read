a81581f86b69498f01f91f060de5eeeb935a06df
vicsantizo <santizo.vic@gmail.com>
2022-12-10 18:40:57 -0600

    chore: add new fields to the form

--- src/createBook/components/BookForm.tsx
+++ src/createBook/components/BookForm.tsx
@@ -4,7 +4,15 @@ import { useBooksStore } from '../../library/dataStores/useBookStore';
 import { useButton } from './hooks/useButton';
 
 export const BookForm = () => {
-  const initialState = { title: '', author: '' };
+  const initialState = {
+    title: '',
+    author: '',
+    category: 'None',
+    description: '',
+    pages: 0,
+    isFavorite: false,
+    isFinished: false,
+  };
   const [bookFields, setBookFields] = useState(initialState);
   const titleInput = useRef<HTMLInputElement | null>(null);
   const { createBook } = useBooksStore();
@@ -15,10 +23,24 @@ export const BookForm = () => {
 
   const { buttonState, setButtonState, ActionKind } = useButton();
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setBookFields({
       ...bookFields,
       [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setBookFields({
+      ...bookFields,
+      [e.target.name]: e.target.options[e.target.selectedIndex].value,
+    });
+  };
+
+  const handleCheckboxChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBookFields({
+      ...bookFields,
+      [e.target.name]: e.target.checked,
     });
   };
 
@@ -27,7 +49,15 @@ export const BookForm = () => {
     if (bookFields.title !== '' && bookFields.author !== '') {
       try {
         setButtonState({ type: ActionKind.LOADING });
-        await createBook(bookFields.title, bookFields.author);
+        await createBook(
+          bookFields.title,
+          bookFields.author,
+          bookFields.description,
+          bookFields.category,
+          bookFields.pages,
+          bookFields.isFavorite,
+          bookFields.isFinished,
+        );
         setButtonState({ type: ActionKind.SUCCESS });
         setTimeout(() => {
           setButtonState({ type: ActionKind.INPUT });
@@ -49,28 +79,103 @@ export const BookForm = () => {
     <form className="relative flex flex-col mx-auto p-[var(--page-padding)] w-[100%] max-w-[400px] h-[90vh]">
       <h1 className="font-bold text-lg mb-6 mt-8">Add Book</h1>
       <label className="text-sm mb-1" htmlFor="title">
-        Title
+        Title *
       </label>
       <input
         ref={titleInput}
         onChange={handleChange}
         value={bookFields.title}
-        className="input input-dark mb-3"
+        className="input input-dark mb-3 rounded-md"
         id="title"
         name="title"
         autoComplete="off"
       />
       <label className="text-sm mb-1" htmlFor="author">
-        Author
+        Author *
       </label>
       <input
         onChange={handleChange}
         value={bookFields.author}
-        className="input input-dark mb-3"
+        className="input input-dark mb-3 rounded-md"
         id="author"
         name="author"
         autoComplete="off"
       />
+      <label className="text-sm mb-1" htmlFor="category">
+        Category
+      </label>
+      <select
+        onChange={handleSelectChange}
+        defaultValue={'None'}
+        name="category"
+        id="category"
+        multiple={false}
+        className="input-dark rounded-md mb-3 py-2 px-1"
+      >
+        <option value="None">None</option>
+        <option value="Programming">Programming</option>
+        <option value="Biography">Biography</option>
+        <option value="Business">Business</option>
+        <option value="Fiction">Fiction</option>
+        <option value="Self-help">Self-help</option>
+        <option value="Philosophy">Philosophy</option>
+      </select>
+      <label className="text-sm mb-1" htmlFor="description">
+        Description
+      </label>
+      <textarea
+        maxLength={600}
+        autoComplete="off"
+        placeholder="Type a brief description of the book..."
+        className="input input-dark rounded-md mb-3 resize-y min-h-[4.5rem]"
+        name="description"
+        id="description"
+        cols={30}
+        rows={5}
+        value={bookFields.description}
+        onChange={handleChange}
+      ></textarea>
+      <label className="text-sm mb-1" htmlFor="pages">
+        Pages
+      </label>
+      <input
+        inputMode="numeric"
+        pattern="\d*"
+        type="number"
+        min={1}
+        className="input input-dark mb-3 rounded-md"
+        id="pages"
+        name="pages"
+        autoComplete="off"
+        value={bookFields.pages}
+        onChange={handleChange}
+      />
+      <div className="flex items-center gap-2 mb-3 mt-3">
+        <input
+          className="checkbox-bg-dark border border-gray-500 outline-none h-[1.5rem] w-[1.5rem] md:h-[1.25rem] md:w-[1.25rem] focus:outline-[#3d65af]"
+          type="checkbox"
+          id="isFavorite"
+          name="isFavorite"
+          checked={bookFields.isFavorite}
+          onChange={handleCheckboxChange}
+        />
+        <label className="text-sm" htmlFor="isFavorite">
+          Mark as favorite
+        </label>
+      </div>
+      <div className="flex items-center gap-2 mb-3">
+        <input
+          className="checkbox-bg-dark border border-gray-500 outline-none h-[1.5rem] w-[1.5rem] md:h-[1.25rem] md:w-[1.25rem] focus:outline-[#3d65af]"
+          type="checkbox"
+          id="isFinished"
+          name="isFinished"
+          checked={bookFields.isFinished}
+          onChange={handleCheckboxChange}
+        />
+        <label className="text-sm" htmlFor="isFinished">
+          Mark as finished
+        </label>
+      </div>
       <button
         disabled={buttonState.disabled}
         onClick={handleClick}
