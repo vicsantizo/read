edff047da7de1e497ef31b5385c6844867ecfb87
vicsantizo <santizo.vic@gmail.com>
2022-12-10 18:24:54 -0600

    wrap SVG into a button element

--- src/toggle/components/Toggle.tsx
+++ src/toggle/components/Toggle.tsx
@@ -15,24 +15,17 @@ export function Toggle(props: ToggleProps) {
       <span role="presentation" className="toggle__label no-selection" onClick={handleSetIsActive}>
         {props.text}
       </span>
-      <svg
-        className="toggle__svg"
-        role="switch"
-        onClick={handleSetIsActive}
-        aria-label={props.text}
-        aria-checked={isActive}
-        xmlns="http://www.w3.org/2000/svg"
-        width={width}
-        height={height}
-      >
-        <rect className={`toggle__rect ${isActive && 'rect--move'}`} width={width} height={height} rx={height / 2} />
-        <circle
-          className={`toggle__circle ${isActive && 'circle--move'}`}
-          cx={height / 2}
-          cy={height / 2}
-          r={height / 2}
-        />
-      </svg>
+      <button onClick={handleSetIsActive} role="switch" aria-label={props.text} aria-checked={isActive}>
+        <svg className="toggle__svg" xmlns="http://www.w3.org/2000/svg" width={width} height={height}>
+          <rect className={`toggle__rect ${isActive && 'rect--move'}`} width={width} height={height} rx={height / 2} />
+          <circle
+            className={`toggle__circle ${isActive && 'circle--move'}`}
+            cx={height / 2}
+            cy={height / 2}
+            r={height / 2}
+          />
+        </svg>
+      </button>
     </div>
   );
 }
