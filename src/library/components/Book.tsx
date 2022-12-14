43292afc791ea7f85ee648fcff5ebe45ee62ff90
vicsantizo <santizo.vic@gmail.com>
2022-12-13 23:37:40 -0600

    chore: set theme to normal for books

--- src/library/components/Book.tsx
+++ src/library/components/Book.tsx
@@ -14,11 +14,9 @@ export const Book = (props: BookProps) => {
   const { title, author, bookId, editionMode, booksSelected } = props;
   return (
     <div className="book relative">
-      <div className="absolute top-0 right-0 mr-2 mt-2 flex items-center gap-2">
+      <div className="absolute top-0 right-0 mr-2 mt-2 flex flex-col items-center gap-1 z-[3]">
         <input
-          className={`${
-            editionMode ? 'visible' : 'invisible'
-          } h-[1.5rem] w-[1.5rem] md:h-[1.25rem] md:w-[1.25rem] z-[3] mr-auto`}
+          className={`${editionMode ? 'visible' : 'invisible'} h-[1.5rem] w-[1.5rem] md:h-[1.2rem] md:w-[1.2rem] z-[3]`}
           type={'checkbox'}
           onChange={(e) => {
             booksSelected.current[bookId] = e.target.checked;
