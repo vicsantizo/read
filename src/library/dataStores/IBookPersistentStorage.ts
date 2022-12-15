b737883167d6f82696df35f9fce7508a14a98fe1
vicsantizo <santizo.vic@gmail.com>
2022-12-15 01:40:37 -0600

    chore: Add `fetchBookById` function definition to interface

--- src/library/dataStores/IBookPersistentStorage.ts
+++ src/library/dataStores/IBookPersistentStorage.ts
@@ -2,6 +2,7 @@ import { Book } from '../models';
 
 export interface IBookPersistentStorage {
   fetchAllBooks: () => Promise<Book[]>;
+  fetchBookById: (id: string) => Promise<Book> | undefined;
   saveAllBooks: (books: Book[]) => Promise<void>;
   deleteBook: (ids: Record<string, boolean>) => Promise<Book[]>;
 }
