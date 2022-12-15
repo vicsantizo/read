4c20d68352b8719efa4038daf242ad18f9448ffc
vicsantizo <santizo.vic@gmail.com>
2022-12-15 01:53:01 -0600

    chore: replace checkbox selection for direct book selection by clicking

--- src/library/components/Book.tsx
+++ src/library/components/Book.tsx
@@ -1,4 +1,4 @@
-import { MutableRefObject } from 'react';
+import { MutableRefObject, useRef } from 'react';
 import './css/book.css';
 import { cutString } from '../../helper';
 
@@ -6,26 +6,24 @@ export type BookProps = {
   title: string;
   author: string;
   bookId: string;
-  booksSelected: MutableRefObject<Record<string, boolean>>;
-  editionMode: boolean;
+  booksSelected: Record<string, boolean>;
 };
 
 export const Book = (props: BookProps) => {
-  const { title, author, bookId, editionMode, booksSelected } = props;
+  const { title, author, bookId, booksSelected } = props;
+  const bookButton = useRef<HTMLButtonElement | null>(null);
   return (
-    <div className="book relative">
-      <div className="absolute top-0 right-0 mr-2 mt-2 flex flex-col items-center gap-1 z-[3]">
-        <input
-          className={`${editionMode ? 'visible' : 'invisible'} h-[1.5rem] w-[1.5rem] md:h-[1.2rem] md:w-[1.2rem] z-[3]`}
-          type={'checkbox'}
-          onChange={(e) => {
-            booksSelected.current[bookId] = e.target.checked;
-          }}
-        />
-      </div>
+    <button
+      ref={bookButton}
+      className="book relative"
+      onClick={(e) => {
+        bookButton.current?.classList.toggle('border');
+        booksSelected[bookId] = !booksSelected[bookId];
+      }}
+    >
       <span className="book__title">{cutString(title, 65)}</span>
       <span className="book__author">{cutString(author, 25)}</span>
-    </div>
+    </button>
   );
 };
 
