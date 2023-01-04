4df7dbb23fc1094866815a1d6e88f8b5fe57e2a5
vicsantizo <santizo.vic@gmail.com>
2023-01-04 14:38:32 -0600

    chore: add callback prop to allow toggle switch functionality

--- src/components/ui/toggle/Toggle.tsx
+++ src/components/ui/toggle/Toggle.tsx
@@ -3,18 +3,25 @@ import './toggle.css';
 
 type ToggleProps = {
   text: string;
+  execute?: () => void;
 };
 
 export const Toggle = (props: ToggleProps) => {
   const { isActive, toggle } = useToggle();
   const width = 40;
   const height = 18;
+
+  const handleClick = () => {
+    toggle();
+    if (props.execute) props.execute();
+  };
+
   return (
     <div className="toggle">
-      <span role="presentation" className="toggle__label no-selection" onClick={toggle}>
+      <span role="presentation" className="toggle__label no-selection" onClick={handleClick}>
         {props.text}
       </span>
-      <button onClick={toggle} role="switch" aria-label={props.text} aria-checked={isActive}>
+      <button onClick={handleClick} role="switch" aria-label={props.text} aria-checked={isActive}>
         <svg className="toggle__svg" xmlns="http://www.w3.org/2000/svg" width={width} height={height}>
           <rect className={`toggle__rect ${isActive && 'rect--move'}`} width={width} height={height} rx={height / 2} />
           <circle
