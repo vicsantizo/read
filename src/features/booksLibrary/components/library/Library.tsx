3b2c30d6fb828b140260d16004d274b1aa1ea196
vicsantizo <santizo.vic@gmail.com>
2023-01-08 13:33:22 -0600

    chore: Add buttons' disabled property and links

--- src/features/booksLibrary/components/library/Library.tsx
+++ src/features/booksLibrary/components/library/Library.tsx
@@ -5,13 +5,12 @@ import { BooksLibraryStore } from '../../../../store/useBooksLibraryStore';
 import { useLibraryFilter } from './useLibraryFilter';
 import { useTheme } from '../../../../context/theme/useTheme';
 import { TrackButton, AddButton, EditButton, DeleteButton } from '../buttons';
+import { Link } from 'react-router-dom';
 
 export const Library = ({ books }: BooksLibraryStore) => {
   const [searchValue, setSeachValue] = useState<string>('');
   const { theme } = useTheme();
   const { filteredBooks } = useLibraryFilter(searchValue, books);
-
-  const libraryBorder = theme === 'dark' ? '#242526' : '#f3f3f3';
 
   return (
     <div className={`library ${theme}`}>
@@ -22,15 +21,21 @@ export const Library = ({ books }: BooksLibraryStore) => {
       <div className="relative flex mb-2 items-center mt-5">
         <h1 className="font-bold text-[1.25rem] text-center sm:mx-auto">My Library</h1>
         <span className="flex items-center absolute gap-2 right-0">
-          <TrackButton />
-          <AddButton />
-          <EditButton />
-          <DeleteButton />
+          <TrackButton disabled={true} />
+          <Link className="flex items-center" to="books/create">
+            <AddButton />
+          </Link>
+          <EditButton disabled={true} />
+          <DeleteButton disabled={true} />
         </span>
       </div>
 
       <div className="library__body">
-        <div className={`border border-[${libraryBorder}] py-[1rem] flex gap-3 flex-wrap justify-center `}>
+        <div
+          className={`border ${
+            theme == 'dark' ? 'border-[#242526]' : 'border-[#f3f3f3]'
+          } py-[1rem] flex gap-3 flex-wrap justify-center `}
+        >
           {filteredBooks.map((book) => (
             <Book key={book.getIdentifier()} title={book.getTitle()} author={book.getAuthor()} progress={100} />
           ))}
