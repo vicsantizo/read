3e3532842d6a2e0d3bcb5c3f69d201026f04e994
vicsantizo <santizo.vic@gmail.com>
2023-01-12 17:59:49 -0600

    feature: defined class for book tracker

---
+++ src/features/booksLibrary/models/tracker.ts
@@ -0,0 +1,47 @@
+import { v4 as uuid } from 'uuid';
+
+export type TrackerData = {
+  date: Date;
+  fromPage: number;
+  toPage: number;
+  id?: string;
+};
+
+export class Tracker {
+  private log: TrackerData[];
+
+  static serialize = (tracker: Tracker) => {
+    return tracker.log;
+  };
+
+  static deserialize = (serializedTracker: TrackerData[]) => {
+    const tracker = new Tracker();
+    for (const element of serializedTracker) {
+      tracker.insertRecord(element);
+    }
+  };
+
+  constructor(initialData?: TrackerData[]) {
+    this.log = initialData ?? [];
+  }
+
+  insertRecord({ date, fromPage, toPage }: TrackerData) {
+    this.log.push({
+      id: uuid(),
+      date,
+      fromPage,
+      toPage,
+    });
+    return this;
+  }
+
+  getRecentRecords() {
+    const start = this.log.length - 5 < 0 ? 0 : this.log.length - 5;
+    const end = this.log.length;
+    return this.log.slice(start, end);
+  }
+
+  getAllRecords() {
+    return this.log;
+  }
+}
