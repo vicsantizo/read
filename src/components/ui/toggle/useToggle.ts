e41c902fc3ea6bccd879b62c704b18c039ec0f62
vicsantizo <santizo.vic@gmail.com>
2023-01-24 07:39:20 -0600

    refactor: remove unnecessary effect
    Remove unneccessary effect by taking the `defaultState` as the initial value for the toggle state

--- src/components/ui/toggle/useToggle.ts
+++ src/components/ui/toggle/useToggle.ts
@@ -1,12 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-export const useToggle = (defaultState?: boolean) => {
-  useEffect(() => {
-    if (defaultState) setIsActive(defaultState);
-    else setIsActive(false);
-  }, []);
-
-  const [isActive, setIsActive] = useState<boolean>(true);
+export const useToggle = (defaultState = false) => {
+  const [isActive, setIsActive] = useState<boolean>(defaultState);
 
   return {
     isActive,
