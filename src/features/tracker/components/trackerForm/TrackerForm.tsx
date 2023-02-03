48c818b3e0a064f615aa0556be6df6def1ec1192
vicsantizo <santizo.vic@gmail.com>
2023-02-02 22:56:49 -0600

    chore: change tracker form UI

--- src/features/tracker/components/trackerForm/TrackerForm.tsx
+++ src/features/tracker/components/trackerForm/TrackerForm.tsx
@@ -39,8 +39,8 @@ export const TrackerForm = ({ bookData }: TrackerFormProps) => {
       className={`relative mx-auto flex h-[90vh] w-[100%] max-w-[400px] flex-col`}
     >
       <h1 className="mb-8 mt-8 text-center text-lg font-bold text-gray-500">Reading Tracker</h1>
-      <div className="mb-8 flex justify-center gap-5">
-        <div className="flex">
+      <div className="mb-3 flex justify-center gap-5">
+        <div className="flex flex-col">
           {/* Verifying data has been set */}
           {book && (
             <Book
@@ -53,6 +53,7 @@ export const TrackerForm = ({ bookData }: TrackerFormProps) => {
               setBooksSelection={(id: string) => id}
             />
           )}
+          <span className="mt-2 w-full text-center text-sm text-gray-500">{book?.getPages()} pages</span>
         </div>
       </div>
       <div className="mb-8 flex flex-wrap justify-center gap-2">
@@ -63,52 +64,62 @@ export const TrackerForm = ({ bookData }: TrackerFormProps) => {
         <StatCard title="Started" content={book?.getTracker().getStartDate()} />
         <StatCard title="Finished" content={book?.getTracker().getCompletionDate(book?.getPages())} />
       </div>
-      <div className="mb-5 flex w-[100%] flex-col justify-start">
-        <div className="mb-5 flex items-center gap-2">
-          <span className="text-gray-500">{book?.getPages()}</span>
-          <span className="mx-2 h-full border border-l-0 border-gray-500"></span>
-          <input
-            value={formFields.from}
-            onChange={(e) => {
-              setFormFields({ type: FormAction.UPDATE, field: 'from', value: e.target.value });
-            }}
-            required
-            placeholder="From"
-            inputMode="numeric"
-            type="number"
-            className={`w-full rounded-md border border-gray-500 px-2 py-1 ${theme == 'dark' && 'bg-[#1f1f23]'}`}
-            name="from"
-            id="from"
-          />
-          <span className="flex items-center text-xl text-gray-400">-</span>
-          <input
-            value={formFields.to}
-            onChange={(e) => {
-              setFormFields({ type: FormAction.UPDATE, field: 'to', value: e.target.value });
-            }}
-            required
-            inputMode="numeric"
-            placeholder="To"
-            className={`w-full rounded-md border border-gray-500 px-2 py-1 ${theme == 'dark' && 'bg-[#1f1f23]'}`}
-            type="number"
-            name="to"
-            id="to"
-          />
+      <div className="mb-10 flex w-[100%] flex-col justify-start border border-r-0 border-l-0 border-gray-700 py-8">
+        <h2 className="mb-2 text-sm font-bold">Log Progress</h2>
+        <p className="mb-5 text-sm text-gray-500">Enter the date and the range of pages that you have read</p>
+        <div className="mb-7 flex items-center justify-between gap-2">
+          <div className="flex w-full flex-col">
+            <input
+              id="date"
+              name="date"
+              type="date"
+              value={formFields.date}
+              required
+              onChange={(e) => {
+                setFormFields({ type: FormAction.UPDATE, field: 'date', value: e.target.value });
+              }}
+              className={`w-full rounded-md border border-gray-500 px-2 py-1 ${theme == 'dark' && 'bg-[#1f1f23]'}`}
+            />
+          </div>
+          <span className="mx-3 h-full border border-r-0 border-gray-500"></span>
+          <div className="flex items-center">
+            <div className="min-w-[7ch] max-w-[10ch]">
+              <input
+                value={formFields.from}
+                onChange={(e) => {
+                  setFormFields({ type: FormAction.UPDATE, field: 'from', value: e.target.value });
+                }}
+                required
+                placeholder="From"
+                inputMode="numeric"
+                type="number"
+                className={`w-full rounded-md border border-gray-500 px-2 py-1 ${theme == 'dark' && 'bg-[#1f1f23]'}`}
+                name="from"
+                id="from"
+                min="0"
+              />
+            </div>
+            <span className="mx-2 flex items-center text-xl text-gray-500">-</span>
+            <div className="min-w-[7ch] max-w-[10ch]">
+              <input
+                value={formFields.to}
+                onChange={(e) => {
+                  setFormFields({ type: FormAction.UPDATE, field: 'to', value: e.target.value });
+                }}
+                required
+                inputMode="numeric"
+                placeholder="To"
+                className={`w-full rounded-md border border-gray-500 px-2 py-1 ${theme == 'dark' && 'bg-[#1f1f23]'}`}
+                type="number"
+                min="0"
+                name="to"
+                id="to"
+              />
+            </div>
+          </div>
         </div>
-        <div className="mb-5 flex gap-3">
-          <input
-            id="date"
-            name="date"
-            type="date"
-            value={formFields.date}
-            required
-            onChange={(e) => {
-              setFormFields({ type: FormAction.UPDATE, field: 'date', value: e.target.value });
-            }}
-            style={{ flexGrow: 1 }}
-            className={`rounded-md border border-gray-500 px-2 py-1 ${theme == 'dark' && 'bg-[#1f1f23]'}`}
-          />
-          <div className="w-[35%]">
+        <div className="flex gap-3">
+          <div className="w-full">
             <AcceptButton
               cleanup={() => {
                 setFormFields({ type: FormAction.WIPE, field: '', value: '' });
@@ -136,6 +147,7 @@ export const TrackerForm = ({ bookData }: TrackerFormProps) => {
         </div>
       </div>
       <div className="form__table h-full">
+        <h2 className="mb-3 text-center text-sm font-bold">History</h2>
         <LogsTable trackingData={book?.getTracker().getAllRecords().slice(-5)} />
       </div>
       <Link
