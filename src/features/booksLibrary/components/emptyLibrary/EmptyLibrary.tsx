53d5c47ebad6aa18e3c94bf248b7d6ff478ea4dd
vicsantizo <santizo.vic@gmail.com>
2023-02-04 21:51:31 -0600

    chore: center component in the middle of the page

--- src/features/booksLibrary/components/emptyLibrary/EmptyLibrary.tsx
+++ src/features/booksLibrary/components/emptyLibrary/EmptyLibrary.tsx
@@ -2,17 +2,16 @@ import { Link } from 'react-router-dom';
 
 export const EmptyLibrary = () => {
   return (
-    <div className="flex h-full flex-col items-center justify-center gap-4">
+    <div className="mt-auto flex h-[80vh] h-full flex-col items-center justify-center gap-4">
       <img
-        loading="lazy"
         src="https://raw.githubusercontent.com/vicsantizo/remote/main/images/students-library.png"
         width={250}
         height={197}
         alt=""
       />
       <div className="text-center text-sm">
-        <div className="text-base font-bold">You do not have any books yet.</div>
-        <div className="text-gray-500">Add a new book that you&apos;d like to track</div>
+        <div className="text-base font-bold">You don&apos;t have any books yet</div>
+        <div className="text-gray-500">Add a new book that you would like to track</div>
       </div>
       <div className="w-[120px] rounded-md bg-[var(--success-500)] py-2 pl-2 pr-3 hover:opacity-50">
         <Link to={'/books/create'} className="flex items-center text-center text-sm font-bold">
