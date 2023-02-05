6a235b0695e4f708ca14966aedb9e2755eca0430
vicsantizo <santizo.vic@gmail.com>
2023-02-04 21:53:20 -0600

    chore: don't render disabled actions when the library is empty

--- src/features/booksLibrary/components/library/Library.tsx
+++ src/features/booksLibrary/components/library/Library.tsx
@@ -27,51 +27,57 @@ export const Library = ({ books, deleteBookById }: BooksLibraryStore) => {
 
   return (
     <div className={`library ${theme}`}>
-      <div className="library__filter mb-12 flex items-center justify-center pt-[4rem]">
-        <SearchInput searchValue={searchValue} setSearchValue={setSearchValue} />
-      </div>
+      {books.length === 0 ? (
+        <EmptyLibrary />
+      ) : (
+        <>
+          <div className="library__filter mb-12 flex items-center justify-center pt-[4rem]">
+            <SearchInput searchValue={searchValue} setSearchValue={setSearchValue} />
+          </div>
 
-      <div className="relative mb-2 mt-5 flex items-center">
-        <h1 className="text-center text-[1.25rem] font-bold text-gray-600 dark:text-gray-400 sm:mx-auto">My Library</h1>
-        <span className="absolute right-0 flex h-full items-center gap-2">
-          <AddButton to="books/create" />
-          <span className="h-full border border-l-0 border-gray-500"></span>
-          <TrackButton to={`books/${getSelectedBook()}/track`} disabled={!(countElementsSelected() === 1)} />
-          <EditButton to={`books/${getSelectedBook()}/edit`} disabled={!(countElementsSelected() === 1)} />
-          <DeleteButton
-            disabled={!(countElementsSelected() >= 1)}
-            execute={() => {
-              deleteBookById(getArrayOfCurrentlySelectedBooks());
-              resetBooksSelection();
-            }}
-          />
-        </span>
-      </div>
+          <div className="relative mb-2 mt-5 flex items-center">
+            <h1 className="text-center text-[1.25rem] font-bold sm:mx-auto">My Library</h1>
+            <span className="absolute right-0 flex h-full items-center gap-2">
+              <AddButton to="books/create" />
+              <span className="h-full border border-l-0 border-gray-500"></span>
+              <TrackButton to={`books/${getSelectedBook()}/track`} disabled={!(countElementsSelected() === 1)} />
+              <EditButton to={`books/${getSelectedBook()}/edit`} disabled={!(countElementsSelected() === 1)} />
+              <DeleteButton
+                disabled={!(countElementsSelected() >= 1)}
+                execute={() => {
+                  deleteBookById(getArrayOfCurrentlySelectedBooks());
+                  resetBooksSelection();
+                }}
+              />
+            </span>
+          </div>
 
-      <div className="library__body">
-        <div
-          className={`border ${
-            theme == 'dark' ? 'border-[#242526]' : 'border-[#f3f3f3]'
-          } flex flex-wrap justify-center gap-3 py-[1rem] `}
-        >
-          {filteredBooks.map((book) => (
-            <Book
-              booksSelection={booksSelection}
-              setBooksSelection={setBooksSelection}
-              resetBooksSelection={resetBooksSelection}
-              deleteBookById={deleteBookById}
-              key={book.getIdentifier()}
-              id={book.getIdentifier()}
-              title={book.getTitle()}
-              author={book.getAuthor()}
-              enableActions={true}
-              progress={book?.getTracker().calculateCompletion(book.getPages())}
-            />
-          ))}
-          {books.length === 0 && <EmptyLibrary />}
-          {books.length > 0 && filteredBooks.length === 0 && 'No results...'}
-        </div>
-      </div>
+          <div className="library__body">
+            <div
+              className={`border ${
+                theme == 'dark' ? 'border-[#242526]' : 'border-[#f3f3f3]'
+              } flex flex-wrap justify-center gap-3 py-[1rem] `}
+            >
+              {filteredBooks.map((book) => (
+                <Book
+                  booksSelection={booksSelection}
+                  setBooksSelection={setBooksSelection}
+                  resetBooksSelection={resetBooksSelection}
+                  deleteBookById={deleteBookById}
+                  key={book.getIdentifier()}
+                  id={book.getIdentifier()}
+                  title={book.getTitle()}
+                  author={book.getAuthor()}
+                  enableActions={true}
+                  progress={book?.getTracker().calculateCompletion(book.getPages())}
+                />
+              ))}
+              {/* {books.length === 0 && <EmptyLibrary />} */}
+              {books.length > 0 && filteredBooks.length === 0 && 'No results...'}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
