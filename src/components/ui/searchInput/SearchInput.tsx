3821960eb25fbeabc0b19152f151376029bb4d03
vicsantizo <santizo.vic@gmail.com>
2023-02-05 21:21:19 -0600

    chore: add "well" effect to search input

--- src/components/ui/searchInput/SearchInput.tsx
+++ src/components/ui/searchInput/SearchInput.tsx
@@ -15,18 +15,20 @@ export const SearchInput = ({ searchValue, setSearchValue }: SearchProps) => {
   }
 
   return (
-    <div className="relative">
+    <div className="relative w-full min-w-[25ch] max-w-[40ch]">
       <button tabIndex={-1} className="absolute top-[50%] left-[10px] translate-y-[-50%]">
         <SearchIcon />
       </button>
       <input
-        type="search"
+        type="text"
         value={searchValue}
         onChange={handleChange}
         placeholder="Search book..."
-        className={`rounded-3xl ${
-          theme === 'dark' ? 'bg-[#1f1f23]' : 'bg-white'
-        } w-[35ch] min-w-[25ch] border border-gray-500 py-2 pl-10 pr-3 outline-none focus:border-[#3d65af] xl:w-[45ch] 2xl:w-[55ch]`}
+        className={`rounded-md border ${
+          theme === 'dark'
+            ? 'border-l-0 border-r-0 border-t-0 border-gray-800 bg-[#343434] [box-shadow:inset_0_4px_4px_hsl(0_0%_0%_/0.1)]'
+            : 'border-gray-500 bg-white'
+        } w-full  py-2 pl-10 pr-3`}
       />
     </div>
   );
