21ae1c2c1b43aeef139a3c029c14f50f535061fe
vicsantizo <santizo.vic@gmail.com>
2023-02-06 17:36:50 -0600

    chore: change table styling to match the UI

--- src/features/tracker/components/logsTable/LogsTable.tsx
+++ src/features/tracker/components/logsTable/LogsTable.tsx
@@ -19,21 +19,21 @@ function formatDate(date: string) {
 export const LogsTable = ({ trackingData }: LogsTableProps) => {
   const isTrackingDataEmpty = trackingData?.length === 0;
   const logsTable = (
-    <table className="w-full border-collapse rounded-md border-[var(--primary-500)] text-center text-sm">
-      <thead className="bg-[var(--primary-500)] text-white">
+    <table className="w-full overflow-hidden rounded-xl text-sm [box-shadow:0_0_4px_hsl(0_0%_0%/.2)]">
+      <thead className="border border-t-0 border-l-0 border-r-0 border-[hsl(0_0%_25%/.4)]">
         <tr>
-          <th className="w-[55%] border border-[var(--primary-500)] py-1">Date</th>
-          <th className="w-[22.5%] border border-[var(--primary-500)] py-1">From</th>
-          <th className="w-[22.5%] border border-[var(--primary-500)] py-1">To</th>
+          <th className="w-[55%] py-2 pl-4 pr-2 text-left">Date</th>
+          <th className="w-[22.5%] py-2 px-2 text-right">From</th>
+          <th className="w-[22.5%] py-2 pl-2 pr-4 text-right">To</th>
         </tr>
       </thead>
       <tbody>
         {trackingData?.map((log) => {
           return (
             <tr key={log.id}>
-              <td className="border border-[var(--primary-500)] py-2">{formatDate(log.date)}</td>
-              <td className="border border-[var(--primary-500)] py-2">{log.fromPage}</td>
-              <td className="border border-[var(--primary-500)] py-2">{log.toPage}</td>
+              <td className="py-3 pl-4 pr-2 text-left">{formatDate(log.date)}</td>
+              <td className="py-3 px-2 text-right">{log.fromPage}</td>
+              <td className="py-3 pl-2 pr-4 text-right">{log.toPage}</td>
             </tr>
           );
         })}
@@ -42,8 +42,14 @@ export const LogsTable = ({ trackingData }: LogsTableProps) => {
   );
 
   const emptyLogs = (
-    <div className="flex h-full min-h-[8rem] items-center rounded-md border border-gray-500">
-      <span className="w-full text-center text-sm text-gray-400">No reading history...</span>
+    <div className="flex h-full min-h-[8rem] flex-col items-center justify-center gap-1 rounded-xl [box-shadow:0_0_4px_hsl(0_0%_0%/.2)]">
+      <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" className="fill-gray-500" width={30}>
+        <g>
+          <path d="M1.5 1.25a.75.75 0 0 1 1.5 0v1.851A7 7 0 1 1 1 8a.75.75 0 0 1 1.5 0 5.5 5.5 0 1 0 1.725-4H5.75a.75.75 0 0 1 0 1.5h-3.5a.75.75 0 0 1-.75-.75v-3.5z" />
+          <path d="M8.25 4a.75.75 0 0 1 .75.75v3.763l1.805.802a.75.75 0 0 1-.61 1.37l-2.25-1A.75.75 0 0 1 7.5 9V4.75A.75.75 0 0 1 8.25 4z" />
+        </g>
+      </svg>
+      <span className="w-full text-center text-sm text-gray-500">No reading history...</span>
     </div>
   );
 
