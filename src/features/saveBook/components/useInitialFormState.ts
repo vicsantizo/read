4faab1e078630c12bc74a8be64996853fa6ea3da
vicsantizo <santizo.vic@gmail.com>
2023-02-07 16:30:48 -0600

    fix: show error page when books do not exist

--- src/features/saveBook/components/useInitialFormState.ts
+++ src/features/saveBook/components/useInitialFormState.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useBooksLibraryStore } from '../../../store/useBooksLibraryStore';
 import { FormState } from './useSaveBook';
 
@@ -15,10 +16,12 @@ const initialForm = {
 export const useInitialFormState = (bookId: string) => {
   const [initialFormState, setInitialFormState] = useState<FormState>(initialForm);
   const { getBookById } = useBooksLibraryStore();
+  const navigation = useNavigate();
 
   useEffect(() => {
     getBookById(bookId).then((retrievedBook) => {
       if (retrievedBook === undefined) {
+        navigation('/error');
         throw Error(`The book with the ${bookId} does not exist`);
       }
       setInitialFormState({
--- src/features/tracker/components/trackerForm/useBook.ts
+++ src/features/tracker/components/trackerForm/useBook.ts
@@ -1,14 +1,17 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useBooksLibraryStore } from '../../../../store/useBooksLibraryStore';
 import { Book } from '../../../booksLibrary/models';
 
 export const useBook = (bookId: string) => {
   const [bookData, setBookData] = useState<Book | undefined>(undefined);
   const { getBookById } = useBooksLibraryStore();
+  const navigation = useNavigate();
 
   useEffect(() => {
     getBookById(bookId).then((retrievedBook) => {
       if (retrievedBook === undefined) {
+        navigation('/error'); // temp fix
         throw Error(`The book with the ${bookId} does not exist`);
       }
       setBookData(retrievedBook);
