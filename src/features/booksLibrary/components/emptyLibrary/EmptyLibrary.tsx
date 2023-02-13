a99c0b5f9acf465e44383b4cd3a08ef40c4818e3
vicsantizo <santizo.vic@gmail.com>
2023-02-12 22:21:09 -0600

    chore: increase folder "image" for large screens

--- src/features/booksLibrary/components/emptyLibrary/EmptyLibrary.tsx
+++ src/features/booksLibrary/components/emptyLibrary/EmptyLibrary.tsx
@@ -6,8 +6,8 @@ export const EmptyLibrary = () => {
   return (
     <div className="mt-auto flex h-full flex-col items-center justify-center gap-4">
       <div className="mb-1">
-        <div className="h-[12px] w-[50px] bg-[var(--primary-500)]"></div>
-        <div className="flex h-[80px] w-[120px] flex-col items-center justify-center bg-[var(--primary-500)] text-sm italic leading-tight text-gray-300 [box-shadow:0_0_5px_hsl(0_0%_0%/.7)]">
+        <div className="h-[12px] w-[50px] rounded-sm bg-[var(--primary-500)] md:h-[16px] md:w-[60px]"></div>
+        <div className="flex h-[80px] w-[120px] flex-col items-center justify-center rounded-sm bg-[var(--primary-500)] text-sm italic leading-tight text-gray-300 [box-shadow:0_0_5px_hsl(0_0%_0%/.7)] md:h-[100px] md:w-[165px]">
           books
         </div>
       </div>
