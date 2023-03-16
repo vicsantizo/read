83641ab1defa7d2a8352713d9f0c56a359f43865
vicsantizo <santizo.vic@gmail.com>
2023-03-16 13:52:51 -0600

    chore: move router to app component and define routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { Outlet } from 'react-router';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BaseLayout } from './layouts/baseLayout';
+import { Home } from './pages/Home';
+import { Error404 } from './pages/Error404';
 import './App.css';
+import './assets/css/input.css';
+import './assets/css/button.css';
 
 function App() {
-  return (
-    <div>
-      <div>App</div>
-      <div className="layout">
-        <Outlet />
-      </div>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      element: <BaseLayout />,
+      errorElement: <Error404 />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
 import './index.css';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 );
