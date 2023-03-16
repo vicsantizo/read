8abf5e486f5faf776ebed60ff7ed586caeb1b5aa
vicsantizo <santizo.vic@gmail.com>
2023-03-16 14:21:39 -0600

    chore: add code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import { BaseLayout } from './layouts/baseLayout';
-import { Home } from './pages/Home';
-import { Error404 } from './pages/Error404';
 import './App.css';
 import './assets/css/input.css';
 import './assets/css/button.css';
+
+const Error404 = lazy(() => import('./pages/Error404'));
+const Home = lazy(() => import('./pages/Home'));
 
 function App() {
   const router = createBrowserRouter([
     {
       element: <BaseLayout />,
-      errorElement: <Error404 />,
+      errorElement: (
+        <Suspense fallback="Loading...">
+          <Error404 />
+        </Suspense>
+      ),
       children: [
         {
           path: '/',
-          element: <Home />,
+          element: (
+            <Suspense fallback="Loading...">
+              <Home />
+            </Suspense>
+          ),
         },
       ],
     },
