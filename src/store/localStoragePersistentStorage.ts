7bcc572556df8a2a53dfc6639e804645e3b57ed6
vicsantizo <santizo.vic@gmail.com>
2023-03-20 20:10:24 -0600

    feat: implement local storage persistent storage

---
+++ src/store/localStoragePersistentStorage.ts
@@ -0,0 +1,63 @@
+import { Book, SerializedBook } from '../features/BooksLibrary/models/book';
+import { IBooksLibraryPersistentStorage } from './IBooksLibraryPersistentStorage';
+
+export class LocalStoragePersistentStorage implements IBooksLibraryPersistentStorage {
+  private key = 'books';
+
+  constructor() {
+    localStorage.setItem(this.key, '[]');
+  }
+
+  private saveBooks(books: Book[]) {
+    const serializedBooks = books.map((books: Book) => Book.serialize(books));
+    localStorage.setItem(this.key, JSON.stringify(serializedBooks));
+  }
+
+  getAllBooks(): Book[] {
+    const localStorageBooks = JSON.parse(localStorage.getItem(this.key) ?? '[]');
+    const books = localStorageBooks.map((book: SerializedBook) => Book.deserialize(book));
+    return books;
+  }
+
+  getBook(id: string): false | Book {
+    const books = this.getAllBooks();
+
+    for (const book of books) {
+      if (book.getId() === id) {
+        return book;
+      }
+    }
+
+    return false;
+  }
+
+  addBook(book: Book): boolean {
+    const books = [...this.getAllBooks(), book];
+    this.saveBooks(books);
+    return true;
+  }
+
+  updateBook(id: string, newBook: Book): boolean {
+    const isBookDeleted = this.deleteBook(id);
+
+    if (isBookDeleted) {
+      this.addBook(newBook);
+      return true;
+    }
+
+    return false;
+  }
+
+  deleteBook(id: string): false | Book {
+    const books = this.getAllBooks();
+
+    for (let i = 0; i < books.length; i++) {
+      if (books[i].getId() === id) {
+        const booksWithoutDeletedBook = [...books].splice(i, 1);
+        this.saveBooks(booksWithoutDeletedBook);
+        return books[i];
+      }
+    }
+    return false;
+  }
+}
