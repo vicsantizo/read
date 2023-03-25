19a0ac10a04042f4c66ed6e11f470f65296685db
vicsantizo <santizo.vic@gmail.com>
2023-03-25 08:22:09 -0600

    chore: add max-width and margin utility classes to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
-export const Home = () => {
-  return <div>Home</div>;
+import { Library } from '../features/BooksLibrary/components/library';
+import '../assets/css/homePage.css';
+
+const Home = () => {
+  return (
+    <div className="home container mx-auto">
+      <Library />
+    </div>
+  );
 };
 
 export default Home;
