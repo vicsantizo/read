58320aebcc9e254eb1c7e6f00ad71092f68010bf
vicsantizo <santizo.vic@gmail.com>
2023-03-26 22:58:14 -0600

    feat: create PopoverOptions component to show available book actions

--- src/features/BooksLibrary/components/book/Book.tsx
+++ src/features/BooksLibrary/components/book/Book.tsx
@@ -2,6 +2,9 @@ import { Book as BookType } from '../../models/book';
 import { shortenText } from '../../../../utils/textFormat';
 import { Link } from 'react-router-dom';
 import { BookProgress } from './BookProgress';
+import { OptionsButton } from './OptionsButton';
+import { useState } from 'react';
+import { PopoverOptions } from './PopoverOptions';
 import './book.css';
 
 type BookProps = {
@@ -9,18 +12,26 @@ type BookProps = {
 };
 
 export const Book = ({ book }: BookProps) => {
+  const [showOptionsPopover, setShowOptionsPopover] = useState<boolean>(false);
   const title = shortenText(book.getTitle(), 30);
   const author = shortenText(book.getAuthor(), 30);
+  const id = book.getId();
   const pages = book.getPages();
   const progress = book.getTrackerLog().calculateBookCompletionPercentage(pages);
 
+  const toggleOptionsPopover = () => {
+    setShowOptionsPopover(!showOptionsPopover);
+  };
+
   return (
     <div className="book-container">
+      <OptionsButton execute={toggleOptionsPopover} />
       <Link to={`/books/${book.getId()}`} className="book" aria-label={`Book ${title} by ${author}`}>
         <span className="book__title">{title}</span>
         <span className="book__author">{author}</span>
       </Link>
       <BookProgress progress={progress} />
+      {showOptionsPopover && <PopoverOptions bookId={id} handleClickout={toggleOptionsPopover} />}
     </div>
   );
 };
---
+++ src/features/BooksLibrary/components/book/PopoverOptions.tsx
@@ -0,0 +1,60 @@
+import { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { useDetectClickOut } from '../../../../hooks/useDetectClickOut';
+import { EditIcon, InfoIcon, TrackIcon, DeleteIcon } from '../icons';
+
+type PopoverOptionsProps = {
+  bookId: string;
+  handleClickout: () => void;
+};
+
+export const PopoverOptions = ({ bookId, handleClickout }: PopoverOptionsProps) => {
+  const popoverRef = useRef<HTMLDivElement>(null);
+  useDetectClickOut(popoverRef, handleClickout);
+
+  return (
+    <div ref={popoverRef} className="book__popover book__popover--dark">
+      <ul className="book__popover-list">
+        <li className="book__popover-item book__popover-item--border">
+          <Link to={`/books/${bookId}`} className="book__popover-link">
+            See more
+            <div className="book__popover-link-icon-box">
+              <InfoIcon className="book__popover-link-icon book__popover-link-icon--dark" />
+            </div>
+          </Link>
+        </li>
+
+        <li className="book__popover-item">
+          <Link to={`/books/edit/${bookId}`} className="book__popover-link">
+            Edit book
+            <div className="book__popover-link-icon-box">
+              <EditIcon className="book__popover-link-icon book__popover-link-icon--dark" />
+            </div>
+          </Link>
+        </li>
+
+        <li className="book__popover-separator book__popover-separator--dark"></li>
+
+        <li className="book__popover-item book__popover-item--border">
+          <Link to={`/books/track/${bookId}`} className="book__popover-link">
+            Track progress
+            <div className="book__popover-link-icon-box">
+              <TrackIcon className="book__popover-link-icon book__popover-link-icon--dark" />
+            </div>
+          </Link>
+        </li>
+
+        <li className="book__popover-item">
+          <button className="book__popover-button">
+            Delete book
+            <div className="book__popover-button-icon-box">
+              <DeleteIcon className="book__popover-button-icon book__popover-button-icon--dark" />
+            </div>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default PopoverOptions;
