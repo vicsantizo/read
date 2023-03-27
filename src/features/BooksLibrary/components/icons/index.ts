ceb11909e0788ef5aff3683afa7818f9a96eb832
vicsantizo <santizo.vic@gmail.com>
2023-03-26 22:28:09 -0600

    chore: add index file to import icons

--- src/features/BooksLibrary/components/icons/index.ts
+++ src/features/BooksLibrary/components/icons/index.ts
@@ -1,1 +1,5 @@
 export { AddIcon } from './AddIcon';
+export { DeleteIcon } from './DeleteIcon';
+export { EditIcon } from './EditIcon';
+export { InfoIcon } from './InfoIcon';
+export { TrackIcon } from './TrackIcon';
