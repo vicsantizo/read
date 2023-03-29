4d9367e5818603b13dfa03d063df8f6e8cec8e05
vicsantizo <santizo.vic@gmail.com>
2023-03-28 20:33:09 -0600

    chore: remove alternative text when prop `text` is empty

--- src/components/common/statCard/StatCard.tsx
+++ src/components/common/statCard/StatCard.tsx
@@ -9,7 +9,7 @@ export const StatCard = ({ title, text }: StatCardProps) => {
   return (
     <div className="statcard statcard--dark">
       <h2 className="statcard__title statcard__title--dark">{title}</h2>
-      <p className="statcard__text statcard__text--dark">{text || 'N/A'}</p>
+      <p className="statcard__text statcard__text--dark">{text}</p>
     </div>
   );
 };
