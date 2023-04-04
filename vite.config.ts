acfd6ccb8968886ba03501c93a8a691bca2077de
vicsantizo <santizo.vic@gmail.com>
2023-04-04 15:57:56 -0600

    chore: modify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['linked-dep'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/linked-dep/, /node_modules/],
-      transformMixedEsModules: true,
-    },
-  },
+  // optimizeDeps: {
+  //   include: ['linked-dep'],
+  // },
+  // build: {
+  //   commonjsOptions: {
+  //     include: [/linked-dep/, /node_modules/],
+  //      transformMixedEsModules: true,
+  //   },
+  // },
 });
