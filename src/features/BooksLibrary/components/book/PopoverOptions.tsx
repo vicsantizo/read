cbe89f8e3fc53bd5c56dd5829f555b77b3010a68
vicsantizo <santizo.vic@gmail.com>
2023-04-04 01:42:56 -0600

    chore: modify link to track book pogress

--- src/features/BooksLibrary/components/book/PopoverOptions.tsx
+++ src/features/BooksLibrary/components/book/PopoverOptions.tsx
@@ -20,9 +20,9 @@ export const PopoverOptions = ({ bookId, handleClickout }: PopoverOptionsProps) 
 
   const { theme } = useTheme();
   const popoverTheme = theme == 'dark' ? 'book__popover--dark' : 'book__popover--light';
+  const popoverSeparatorTheme = theme == 'dark' ? 'book__popover-separator--dark' : 'book__popover-separator--light';
   const popoverLinkIconTheme = theme == 'dark' ? 'book__popover-link-icon--dark' : 'book__popover-link-icon--light';
   const popoverBtnIconTheme = theme == 'dark' ? 'book__popover-button-icon--dark' : 'book__popover-button-icon--light';
-  const popoverSeparatorTheme = theme == 'dark' ? 'book__popover-separator--dark' : 'book__popover-separator--light';
 
   return (
     <div ref={popoverRef} className={`book__popover ${popoverTheme}`}>
@@ -48,7 +48,7 @@ export const PopoverOptions = ({ bookId, handleClickout }: PopoverOptionsProps) 
         <li className={`book__popover-separator ${popoverSeparatorTheme}`}></li>
 
         <li className="book__popover-item book__popover-item--border">
-          <Link to={`/books/track/${bookId}`} className="book__popover-link">
+          <Link to={`/books/${bookId}`} className="book__popover-link" state={{ isOpen: true }}>
             Track progress
             <div className="book__popover-link-icon-box">
               <TrackIcon className={`book__popover-link-icon ${popoverLinkIconTheme}`} />
