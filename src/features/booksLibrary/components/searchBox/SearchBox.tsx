193fb9e062589cb0a0309f34d3bbcf2ff8ba6bac
vicsantizo <santizo.vic@gmail.com>
2023-04-05 12:15:49 -0600

    chore: add prop to disable searchbox

--- src/features/booksLibrary/components/searchBox/SearchBox.tsx
+++ src/features/booksLibrary/components/searchBox/SearchBox.tsx
@@ -5,9 +5,10 @@ import './searchBox.css';
 type SearchBoxProps = {
   searchValue: string;
   setSearchValue: (value: string) => void;
+  isDisabled?: boolean;
 };
 
-export const SearchBox = ({ searchValue, setSearchValue }: SearchBoxProps) => {
+export const SearchBox = ({ searchValue, setSearchValue, isDisabled = false }: SearchBoxProps) => {
   const { theme } = useTheme();
   const inputTheme = theme == 'dark' ? 'input--dark' : 'input--light';
 
@@ -17,9 +18,10 @@ export const SearchBox = ({ searchValue, setSearchValue }: SearchBoxProps) => {
         <SearchIcon />
         <input
           type="search"
+          disabled={isDisabled}
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
-          placeholder="Search book..."
+          placeholder={`${!isDisabled ? 'Search book' : 'No books in your library to search...'}`}
           className={`input search__input ${inputTheme}`}
         />
       </div>
