9fe84801442cb354991896f7be7cf3fadc9f4502
vicsantizo <santizo.vic@gmail.com>
2023-04-06 23:42:17 -0600

    chore: remove hamburger menu rotation transition

--- src/components/ui/burger/Burger.tsx
+++ src/components/ui/burger/Burger.tsx
@@ -8,11 +8,21 @@ type BurgerProps = {
 export const Burger = (props: BurgerProps) => {
   const { isActive, action } = props;
 
+  const hamburgerIcon = (
+    <svg className="burger__icon" width={23} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19 4a1 1 0 0 1-1 1H2a1 1 0 0 1 0-2h16a1 1 0 0 1 1 1zm0 6a1 1 0 0 1-1 1H2a1 1 0 1 1 0-2h16a1 1 0 0 1 1 1zm-1 7a1 1 0 1 0 0-2H2a1 1 0 1 0 0 2h16z" />
+    </svg>
+  );
+
+  const closeIcon = (
+    <svg className="burger__close" width={23} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z" />
+    </svg>
+  );
+
   return (
     <button className="burger" onClick={action}>
-      <span className={`burger__bun ${isActive ? 'burger--rotate-full' : ''}`}></span>
-      <span className={`burger__meat ${isActive ? 'burger--rotate' : ''}`}></span>
-      <span className={`burger__bun ${isActive ? 'burger--hide' : ''}`}></span>
+      {isActive ? closeIcon : hamburgerIcon}
     </button>
   );
 };
