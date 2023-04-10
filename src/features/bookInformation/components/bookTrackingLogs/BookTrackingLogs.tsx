a22faf61785ad9864d3040140de98034d1882caf
vicsantizo <santizo.vic@gmail.com>
2023-04-10 17:03:51 -0600

    chore: change background colors

--- src/features/bookInformation/components/bookTrackingLogs/BookTrackingLogs.tsx
+++ src/features/bookInformation/components/bookTrackingLogs/BookTrackingLogs.tsx
@@ -1,5 +1,6 @@
 import { BookTrackerLog } from '../../../booksLibrary/models/bookTrackerLog';
 import { formatDate } from '../../../../utils/dateFormat';
+import { useTheme } from '../../../../context/theme/useTheme';
 import './bookTrackingLogs.css';
 
 type BookTrackingLogsProps = {
@@ -10,6 +11,8 @@ type BookTrackingLogsProps = {
 export const BookTrackingLogs = ({ trackerLog }: BookTrackingLogsProps) => {
   const logs = [...trackerLog.getAllEntries()].reverse();
   const isTrackerLogEmpty = logs.length === 0;
+  const { theme } = useTheme();
+  const tableTheme = theme === 'dark' ? 'table-standard--dark' : 'table-standard--light';
 
   if (isTrackerLogEmpty) {
     return null;
@@ -17,7 +20,7 @@ export const BookTrackingLogs = ({ trackerLog }: BookTrackingLogsProps) => {
 
   return (
     <div className="table-wrapper">
-      <table className="table--alternate-primary table">
+      <table className={`table-standard table ${tableTheme}`}>
         <thead>
           <tr>
             <th>Date</th>
