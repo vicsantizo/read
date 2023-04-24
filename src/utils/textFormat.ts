0af040a73d4ef70947f55afbe0347eda819c16f4
vicsantizo <santizo.vic@gmail.com>
2023-04-23 22:42:45 -0600

    fix: return shortened text starting from the beginning of the string

--- src/utils/textFormat.ts
+++ src/utils/textFormat.ts
@@ -1,4 +1,4 @@
 export function shortenText(text: string, desiredLength: number) {
   if (text.length <= desiredLength) return text;
-  return text.slice(desiredLength).concat('.');
+  return text.slice(0, desiredLength).concat('.');
 }
