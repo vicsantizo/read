61f0c2855aec25568ff998a9b27c55fa89aeaf60
vicsantizo <santizo.vic@gmail.com>
2023-05-04 17:34:30 -0600

    fix: modify `shortenText()` to include the "." as part of the desiredLength

--- src/utils/textFormat.ts
+++ src/utils/textFormat.ts
@@ -1,4 +1,4 @@
 export function shortenText(text: string, desiredLength: number) {
   if (text.length <= desiredLength) return text;
-  return text.slice(0, desiredLength).concat('.');
+  return text.slice(0, desiredLength - 1).concat('.');
 }
